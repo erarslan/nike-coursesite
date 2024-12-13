d5e67a756fbe65a022ad2cbdf53464b63ffe24c1
Emre Erarslan <emrerars@gmail.com>
2024-12-13 18:20:46 +0300

    added curved

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -5,6 +5,11 @@ import { useState, useEffect } from "react";
 const PreLoader = () => {
   const [index, setIndex] = useState(0);
   const words = ["Merhaba", "Selam", "Hi", "Hello", "Hola", "Bonjour", "Hallo"];
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    setDimension({ width: window.innerWidth, height: window.innerHeight });
+  }, []);
 
   useEffect(() => {
     if (index === words.length - 1) return;
@@ -17,6 +22,30 @@ const PreLoader = () => {
     console.log(index);
   }, [index]);
 
+  const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
+    dimension.height
+  } Q${dimension.width / 2} ${dimension.height + 300} 0 ${
+    dimension.height
+  }  L0 0`;
+
+  const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
+    dimension.height
+  } Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`;
+
+  const curve = {
+    initial: {
+      d: initialPath,
+
+      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
+    },
+
+    exit: {
+      d: targetPath,
+
+      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 },
+    },
+  };
+
   return (
     <motion.div
       variants={slideUp}
@@ -24,14 +53,27 @@ const PreLoader = () => {
       exit="exit"
       className="w-screen h-screen flex justify-center items-center fixed z-[99] top-0 left-0 text-white bg-[#141516]"
     >
-      <motion.p
-        variants={opacity}
-        initial="initial"
-        animate="enter"
-        className="text-[42px]"
-      >
-        {words[index]}
-      </motion.p>
+      {dimension.width > 0 && (
+        <>
+          <motion.p
+            variants={opacity}
+            initial="initial"
+            animate="enter"
+            className="z-10 text-[42px]"
+          >
+            <span></span>
+            {words[index]}
+          </motion.p>
+
+          <svg>
+            <motion.path
+              variants={curve}
+              initial="initial"
+              exit="exit"
+            ></motion.path>
+          </svg>
+        </>
+      )}
     </motion.div>
   );
 };
